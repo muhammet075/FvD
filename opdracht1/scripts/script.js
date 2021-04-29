8ff8f951671696540ee42452fb621065aafc9135
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-04-29 03:14:21 +0200

    oreo animatie aangemaakt

--- opdracht1/scripts/script.js
+++ opdracht1/scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("howdy");
+
+var oreologo = document.querySelector("h1");
+
+oreologo.addEventListener("mouseover", beweegKoekje);
+
+function beweegKoekje() {
+    oreologo.classList.add("animatie");
+    oreologo.classList.add("letteranimatie")
+};
