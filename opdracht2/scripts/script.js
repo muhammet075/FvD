cefc38fe6c3bc329a418d68c1011fa90137e8a6a
MUHAMMET KOMUR <muhammet075@icloud.com>
2021-06-04 01:58:24 +0200

    opslaan feature afgemaakt

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,6 +1,8 @@
 // JavaScript Document
 console.log("howdy");
 
+var favorietKnop = document.querySelectorAll(".favorietknop");
+
 var rood = document.querySelectorAll(".rood");
 var groen = document.querySelectorAll(".groen");
 var geel = document.querySelectorAll(".geel");
@@ -34,9 +36,83 @@ var opnieuwKnop = document.querySelector("#opnieuw");
 var kleuren = document.querySelector(".kleuren");
 var landen = document.querySelector(".landen");
 
-var opgeslagenLijst = document.querySelector(".opgeslagenlijst");
 
-//eerste
+var opgeslagenLijst = document.querySelector(".opgeslagenlijst");
+var lijstKnop = document.querySelector(".lijstknop");
+var lijstKnopSluit = document.querySelector(".lijstsluit");
+var favorietLijst = document.querySelector(".opgeslagenitems");
+
+
+
+var favorietTeller = document.querySelector(".aantalfavorieten");
+
+document.addEventListener("click", toonAantal)
+
+function toonAantal() {
+
+    //aantal child elementen tellen in de opgeslagen afbeeldingen lijst
+    var opgeslagenFotos = opgeslagenLijst.childElementCount;
+
+    //als er 1 child element geteld is in de afbeelding lijst wordt er 0 getoond naast het knopje
+    if (opgeslagenFotos === 1) {
+
+        console.log(opgeslagenFotos);
+
+        //0 wordt getoond omdat de p tag waar geen favorieten opgeslagen (zero state) staan ook als child gezien wordt
+        favorietTeller.innerHTML = "0";
+
+        
+        // als het meer dan 1 is wordt er geteld en 1 eraf getrokken (min de zero state bericht)
+    } else {
+        console.log(opgeslagenFotos);
+        favorietTeller.innerHTML = opgeslagenFotos - 1;
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+lijstKnop.addEventListener("click", toonFavorietMenu);
+
+function toonFavorietMenu() {
+    favorietLijst.style.display = "block";
+    favorietLijst.classList.remove("menuanimatie-sluit");
+    favorietLijst.classList.add("menuanimatie");
+}
+
+
+lijstKnopSluit.addEventListener("click", sluitFavorietMenu);
+
+
+function sluitFavorietMenu() {
+    favorietLijst.classList.remove("menuanimatie");
+    favorietLijst.classList.add("menuanimatie-sluit");
+
+    setTimeout(function () {
+        favorietLijst.style.display = "none";
+    }, 1000);
+
+}
+
+window.addEventListener("click", heLa)
+
+function heLa() {
+    if (opgeslagenLijst.childNodes.length > 0) {
+
+
+    } else {
+        console.log("test")
+    }
+}
+
+
 var favorietKnop1 = document.querySelector(
     ".fotolijst section:nth-child(1) .favorietknop"
 );
@@ -194,487 +270,14 @@ var favorietKnop52 = document.querySelector(
     ".fotolijst section:nth-child(52) .favorietknop"
 );
 
-console.log(favorietKnop52);
 
-favorietKnop1.addEventListener("click", favorietOpslaan1);
 
-function favorietOpslaan1() {
-    if (favorietKnop1.classList.contains("klik")) {
-        favorietKnop1.classList.remove("klik");
-        favorietKnop1.style.color = "#fff200";
 
-        var fotoNieuw = document.createElement("img");
 
-        fotoNieuw.src = "./images/fotos/TR/geel/geel3.jpeg";
-        fotoNieuw.classList.add("foto1");
-        opgeslagenLijst.appendChild(fotoNieuw);
-        console.log(fotoNieuw);
-    } else {
-        favorietKnop1.classList.add("klik");
-        favorietKnop1.style.color = "#000";
-        document.querySelector(".foto1").remove();
-    }
-}
 
-favorietKnop2.addEventListener("click", favorietOpslaan2);
 
-function favorietOpslaan2() {
-    if (favorietKnop2.classList.contains("klik")) {
-        favorietKnop2.classList.remove("klik");
-        favorietKnop2.style.color = "#fff200";
 
-        var fotoNieuw2 = document.createElement("img");
 
-        fotoNieuw2.src = "./images/fotos/GR/paars/paars2.jpeg";
-        fotoNieuw2.classList.add("foto2");
-        opgeslagenLijst.appendChild(fotoNieuw2);
-        console.log(fotoNieuw2);
-    } else {
-        favorietKnop2.classList.add("klik");
-        favorietKnop2.style.color = "#000";
-        document.querySelector(".foto2").remove();
-    }
-}
-
-favorietKnop3.addEventListener("click", favorietOpslaan3);
-
-function favorietOpslaan3() {
-    if (favorietKnop3.classList.contains("klik")) {
-        favorietKnop3.classList.remove("klik");
-        favorietKnop3.style.color = "#fff300";
-
-        var fotoNieuw3 = document.createElement("img");
-
-        fotoNieuw3.src = "./images/fotos/TR/rood/rood2.jpeg";
-        fotoNieuw3.classList.add("foto3");
-        opgeslagenLijst.appendChild(fotoNieuw3);
-        console.log(fotoNieuw3);
-    } else {
-        favorietKnop3.classList.add("klik");
-        favorietKnop3.style.color = "#000";
-        document.querySelector(".foto3").remove();
-    }
-}
-
-favorietKnop4.addEventListener("click", favorietOpslaan4);
-
-function favorietOpslaan4() {
-    if (favorietKnop4.classList.contains("klik")) {
-        favorietKnop4.classList.remove("klik");
-        favorietKnop4.style.color = "#fff400";
-
-        var fotoNieuw4 = document.createElement("img");
-
-        fotoNieuw4.src = "./images/fotos/NL/groen/groen3.jpeg";
-        fotoNieuw4.classList.add("foto4");
-        opgeslagenLijst.appendChild(fotoNieuw4);
-        console.log(fotoNieuw4);
-    } else {
-        favorietKnop4.classList.add("klik");
-        favorietKnop4.style.color = "#000";
-        document.querySelector(".foto4").remove();
-    }
-}
-
-favorietKnop5.addEventListener("click", favorietOpslaan5);
-
-function favorietOpslaan5() {
-    if (favorietKnop5.classList.contains("klik")) {
-        favorietKnop5.classList.remove("klik");
-        favorietKnop5.style.color = "#fff500";
-
-        var fotoNieuw5 = document.createElement("img");
-
-        fotoNieuw5.src = "./images/fotos/GR/paars/paars1.jpeg";
-        fotoNieuw5.classList.add("foto5");
-        opgeslagenLijst.appendChild(fotoNieuw5);
-        console.log(fotoNieuw5);
-    } else {
-        favorietKnop5.classList.add("klik");
-        favorietKnop5.style.color = "#000";
-        document.querySelector(".foto5").remove();
-    }
-}
-
-favorietKnop6.addEventListener("click", favorietOpslaan6);
-
-function favorietOpslaan6() {
-    if (favorietKnop6.classList.contains("klik")) {
-        favorietKnop6.classList.remove("klik");
-        favorietKnop6.style.color = "#fff600";
-
-        var fotoNieuw6 = document.createElement("img");
-
-        fotoNieuw6.src = "./images/fotos/TR/geel/geel1.jpeg";
-        fotoNieuw6.classList.add("foto6");
-        opgeslagenLijst.appendChild(fotoNieuw6);
-        console.log(fotoNieuw6);
-    } else {
-        favorietKnop6.classList.add("klik");
-        favorietKnop6.style.color = "#000";
-        document.querySelector(".foto6").remove();
-    }
-}
-
-favorietKnop7.addEventListener("click", favorietOpslaan7);
-
-function favorietOpslaan7() {
-    if (favorietKnop7.classList.contains("klik")) {
-        favorietKnop7.classList.remove("klik");
-        favorietKnop7.style.color = "#fff700";
-
-        var fotoNieuw7 = document.createElement("img");
-
-        fotoNieuw7.src = "./images/fotos/ES/groen/groen2.jpeg";
-        fotoNieuw7.classList.add("foto7");
-        opgeslagenLijst.appendChild(fotoNieuw7);
-        console.log(fotoNieuw7);
-    } else {
-        favorietKnop7.classList.add("klik");
-        favorietKnop7.style.color = "#000";
-        document.querySelector(".foto7").remove();
-    }
-}
-
-favorietKnop8.addEventListener("click", favorietOpslaan8);
-
-function favorietOpslaan8() {
-    if (favorietKnop8.classList.contains("klik")) {
-        favorietKnop8.classList.remove("klik");
-        favorietKnop8.style.color = "#fff800";
-
-        var fotoNieuw8 = document.createElement("img");
-
-        fotoNieuw8.src = "./images/fotos/TR/groen/groen1.jpeg";
-        fotoNieuw8.classList.add("foto8");
-        opgeslagenLijst.appendChild(fotoNieuw8);
-        console.log(fotoNieuw8);
-    } else {
-        favorietKnop8.classList.add("klik");
-        favorietKnop8.style.color = "#000";
-        document.querySelector(".foto8").remove();
-    }
-}
-
-favorietKnop9.addEventListener("click", favorietOpslaan9);
-
-function favorietOpslaan9() {
-    if (favorietKnop9.classList.contains("klik")) {
-        favorietKnop9.classList.remove("klik");
-        favorietKnop9.style.color = "#fff900";
-
-        var fotoNieuw9 = document.createElement("img");
-
-        fotoNieuw9.src = "./images/fotos/GR/blauw/blauw2.jpeg";
-        fotoNieuw9.classList.add("foto9");
-        opgeslagenLijst.appendChild(fotoNieuw9);
-        console.log(fotoNieuw9);
-    } else {
-        favorietKnop9.classList.add("klik");
-        favorietKnop9.style.color = "#000";
-        document.querySelector(".foto9").remove();
-    }
-}
-
-favorietKnop10.addEventListener("click", favorietOpslaan10);
-
-function favorietOpslaan10() {
-    if (favorietKnop10.classList.contains("klik")) {
-        favorietKnop10.classList.remove("klik");
-        favorietKnop10.style.color = "#fff900";
-
-        var fotoNieuw10 = document.createElement("img");
-
-        fotoNieuw10.src = "./images/fotos/NL/rood/rood2.jpeg";
-        fotoNieuw10.classList.add("foto10");
-        opgeslagenLijst.appendChild(fotoNieuw10);
-        console.log(fotoNieuw10);
-    } else {
-        favorietKnop10.classList.add("klik");
-        favorietKnop10.style.color = "#000";
-        document.querySelector(".foto10").remove();
-    }
-}
-
-favorietKnop11.addEventListener("click", favorietOpslaan11);
-
-function favorietOpslaan11() {
-    if (favorietKnop11.classList.contains("klik")) {
-        favorietKnop11.classList.remove("klik");
-        favorietKnop11.style.color = "#fff900";
-
-        var fotoNieuw11 = document.createElement("img");
-
-        fotoNieuw11.src = "./images/fotos/TR/groen/groen2.jpeg";
-        fotoNieuw11.classList.add("foto11");
-        opgeslagenLijst.appendChild(fotoNieuw11);
-        console.log(fotoNieuw11);
-    } else {
-        favorietKnop11.classList.add("klik");
-        favorietKnop11.style.color = "#000";
-        document.querySelector(".foto11").remove();
-    }
-}
-
-favorietKnop12.addEventListener("click", favorietOpslaan12);
-
-function favorietOpslaan12() {
-    if (favorietKnop12.classList.contains("klik")) {
-        favorietKnop12.classList.remove("klik");
-        favorietKnop12.style.color = "#fff900";
-
-        var fotoNieuw12 = document.createElement("img");
-
-        fotoNieuw12.src = "./images/fotos/TR/blauw/blauw1.jpeg";
-        fotoNieuw12.classList.add("foto12");
-        opgeslagenLijst.appendChild(fotoNieuw12);
-        console.log(fotoNieuw12);
-    } else {
-        favorietKnop12.classList.add("klik");
-        favorietKnop12.style.color = "#000";
-        document.querySelector(".foto12").remove();
-    }
-}
-
-favorietKnop13.addEventListener("click", favorietOpslaan13);
-
-function favorietOpslaan13() {
-    if (favorietKnop13.classList.contains("klik")) {
-        favorietKnop13.classList.remove("klik");
-        favorietKnop13.style.color = "#fff900";
-
-        var fotoNieuw13 = document.createElement("img");
-
-        fotoNieuw13.src = "./images/fotos/NL/paars/paars1.jpeg";
-        fotoNieuw13.classList.add("foto13");
-        opgeslagenLijst.appendChild(fotoNieuw13);
-        console.log(fotoNieuw13);
-    } else {
-        favorietKnop13.classList.add("klik");
-        favorietKnop13.style.color = "#000";
-        document.querySelector(".foto13").remove();
-    }
-}
-
-favorietKnop14.addEventListener("click", favorietOpslaan14);
-
-function favorietOpslaan14() {
-    if (favorietKnop14.classList.contains("klik")) {
-        favorietKnop14.classList.remove("klik");
-        favorietKnop14.style.color = "#fff900";
-
-        var fotoNieuw14 = document.createElement("img");
-
-        fotoNieuw14.src = "./images/fotos/FR/groen/groen2.jpeg";
-        fotoNieuw14.classList.add("foto14");
-        opgeslagenLijst.appendChild(fotoNieuw14);
-        console.log(fotoNieuw14);
-    } else {
-        favorietKnop14.classList.add("klik");
-        favorietKnop14.style.color = "#000";
-        document.querySelector(".foto14").remove();
-    }
-}
-
-favorietKnop15.addEventListener("click", favorietOpslaan15);
-
-function favorietOpslaan15() {
-    if (favorietKnop15.classList.contains("klik")) {
-        favorietKnop15.classList.remove("klik");
-        favorietKnop15.style.color = "#fff900";
-
-        var fotoNieuw15 = document.createElement("img");
-
-        fotoNieuw15.src = "./images/fotos/NL/rood/rood1.jpeg";
-        fotoNieuw15.classList.add("foto15");
-        opgeslagenLijst.appendChild(fotoNieuw15);
-        console.log(fotoNieuw15);
-    } else {
-        favorietKnop15.classList.add("klik");
-        favorietKnop15.style.color = "#000";
-        document.querySelector(".foto15").remove();
-    }
-}
-
-favorietKnop16.addEventListener("click", favorietOpslaan16);
-
-function favorietOpslaan16() {
-    if (favorietKnop16.classList.contains("klik")) {
-        favorietKnop16.classList.remove("klik");
-        favorietKnop16.style.color = "#fff900";
-
-        var fotoNieuw16 = document.createElement("img");
-
-        fotoNieuw16.src = "./images/fotos/NL/groen/groen2.jpeg";
-        fotoNieuw16.classList.add("foto16");
-        opgeslagenLijst.appendChild(fotoNieuw16);
-        console.log(fotoNieuw16);
-    } else {
-        favorietKnop16.classList.add("klik");
-        favorietKnop16.style.color = "#000";
-        document.querySelector(".foto16").remove();
-    }
-}
-
-favorietKnop17.addEventListener("click", favorietOpslaan17);
-
-function favorietOpslaan17() {
-    if (favorietKnop17.classList.contains("klik")) {
-        favorietKnop17.classList.remove("klik");
-        favorietKnop17.style.color = "#fff900";
-
-        var fotoNieuw17 = document.createElement("img");
-
-        fotoNieuw17.src = "./images/fotos/FR/geel/geel1.jpeg";
-        fotoNieuw17.classList.add("foto17");
-        opgeslagenLijst.appendChild(fotoNieuw17);
-        console.log(fotoNieuw17);
-    } else {
-        favorietKnop17.classList.add("klik");
-        favorietKnop17.style.color = "#000";
-        document.querySelector(".foto17").remove();
-    }
-}
-
-favorietKnop18.addEventListener("click", favorietOpslaan18);
-
-function favorietOpslaan18() {
-    if (favorietKnop18.classList.contains("klik")) {
-        favorietKnop18.classList.remove("klik");
-        favorietKnop18.style.color = "#fff900";
-
-        var fotoNieuw18 = document.createElement("img");
-
-        fotoNieuw18.src = "./images/fotos/NL/geel/geel1.jpeg";
-        fotoNieuw18.classList.add("foto18");
-        opgeslagenLijst.appendChild(fotoNieuw18);
-        console.log(fotoNieuw18);
-    } else {
-        favorietKnop18.classList.add("klik");
-        favorietKnop18.style.color = "#000";
-        document.querySelector(".foto18").remove();
-    }
-}
-
-favorietKnop19.addEventListener("click", favorietOpslaan19);
-
-function favorietOpslaan19() {
-    if (favorietKnop19.classList.contains("klik")) {
-        favorietKnop19.classList.remove("klik");
-        favorietKnop19.style.color = "#fff900";
-
-        var fotoNieuw19 = document.createElement("img");
-
-        fotoNieuw19.src = "./images/fotos/NL/geel/geel2.png";
-        fotoNieuw19.classList.add("foto19");
-        opgeslagenLijst.appendChild(fotoNieuw19);
-        console.log(fotoNieuw19);
-    } else {
-        favorietKnop19.classList.add("klik");
-        favorietKnop19.style.color = "#000";
-        document.querySelector(".foto19").remove();
-    }
-}
-
-favorietKnop20.addEventListener("click", favorietOpslaan20);
-
-function favorietOpslaan20() {
-    if (favorietKnop20.classList.contains("klik")) {
-        favorietKnop20.classList.remove("klik");
-        favorietKnop20.style.color = "#fff900";
-
-        var fotoNieuw20 = document.createElement("img");
-
-        fotoNieuw20.src = "./images/fotos/TR/paars/paars2.jpeg";
-        fotoNieuw20.classList.add("foto20");
-        opgeslagenLijst.appendChild(fotoNieuw20);
-        console.log(fotoNieuw20);
-    } else {
-        favorietKnop20.classList.add("klik");
-        favorietKnop20.style.color = "#000";
-        document.querySelector(".foto20").remove();
-    }
-}
-
-favorietKnop21.addEventListener("click", favorietOpslaan21);
-
-function favorietOpslaan21() {
-    if (favorietKnop21.classList.contains("klik")) {
-        favorietKnop21.classList.remove("klik");
-        favorietKnop21.style.color = "#fff900";
-
-        var fotoNieuw21 = document.createElement("img");
-
-        fotoNieuw21.src = "./images/fotos/NL/blauw/blauw1.jpeg";
-        fotoNieuw21.classList.add("foto21");
-        opgeslagenLijst.appendChild(fotoNieuw21);
-        console.log(fotoNieuw21);
-    } else {
-        favorietKnop21.classList.add("klik");
-        favorietKnop21.style.color = "#000";
-        document.querySelector(".foto21").remove();
-    }
-}
-
-favorietKnop22.addEventListener("click", favorietOpslaan22);
-
-function favorietOpslaan22() {
-    if (favorietKnop22.classList.contains("klik")) {
-        favorietKnop22.classList.remove("klik");
-        favorietKnop22.style.color = "#fff900";
-
-        var fotoNieuw22 = document.createElement("img");
-
-        fotoNieuw22.src = "./images/fotos/ES/paars/paars1.jpeg";
-        fotoNieuw22.classList.add("foto22");
-        opgeslagenLijst.appendChild(fotoNieuw22);
-        console.log(fotoNieuw22);
-    } else {
-        favorietKnop22.classList.add("klik");
-        favorietKnop22.style.color = "#000";
-        document.querySelector(".foto22").remove();
-    }
-}
-
-favorietKnop23.addEventListener("click", favorietOpslaan23);
-
-function favorietOpslaan23() {
-    if (favorietKnop23.classList.contains("klik")) {
-        favorietKnop23.classList.remove("klik");
-        favorietKnop23.style.color = "#fff900";
-
-        var fotoNieuw23 = document.createElement("img");
-
-        fotoNieuw23.src = "./images/fotos/NL/paars/paars2.jpeg";
-        fotoNieuw23.classList.add("foto23");
-        opgeslagenLijst.appendChild(fotoNieuw23);
-        console.log(fotoNieuw23);
-    } else {
-        favorietKnop23.classList.add("klik");
-        favorietKnop23.style.color = "#000";
-        document.querySelector(".foto23").remove();
-    }
-}
-
-favorietKnop24.addEventListener("click", favorietOpslaan24);
-
-function favorietOpslaan24() {
-    if (favorietKnop24.classList.contains("klik")) {
-        favorietKnop24.classList.remove("klik");
-        favorietKnop24.style.color = "#fff900";
-
-        var fotoNieuw24 = document.createElement("img");
-
-        fotoNieuw24.src = "./images/fotos/GR/groen/groen1.jpeg";
-        fotoNieuw24.classList.add("foto24");
-        opgeslagenLijst.appendChild(fotoNieuw24);
-        console.log(fotoNieuw24);
-    } else {
-        favorietKnop24.classList.add("klik");
-        favorietKnop24.style.color = "#000";
-        document.querySelector(".foto24").remove();
-    }
-}
 
 kleuren.addEventListener("click", toonVolgende);
 
